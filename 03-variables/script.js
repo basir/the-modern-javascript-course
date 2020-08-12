37f90f0d1b376ed5ee81e12ab86a35bac87cb00d
Basir <basir.jafarzadeh@gmail.com>
2020-08-12 19:00:52 +0430

    Video-03-Variables

---
+++ 03-variables/script.js
@@ -0,0 +1,33 @@
+var num = 3;
+console.log(num);
+var name = 'Bob';
+console.log(name);
+let message = 'Hello';
+console.log(message);
+message = 'Bye';
+console.log(message);
+const PI = 3.14;
+console.log(PI);
+// PI = 4;
+// Math operator on variables
+let a = 2;
+a = a + 3;
+console.log(a);
+a += 3;
+console.log(a);
+
+a++; // a +=1;
+console.log(a);
+a--;
+console.log(a);
+// String Concatenation
+const myName = 'Basir';
+const myMessage = 'Hello ' + myName;
+console.log(myMessage);
+// String Interpolation
+const fullMessage = `My full ${myMessage} message `;
+console.log(fullMessage);
+
+// typeof operator
+console.log(typeof fullMessage);
+console.log(typeof num);
